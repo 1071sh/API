93d38fee5e39ae91dd4804ea75c8577104443f3d
1071sh <i9i9ksy@gmail.com>
2025-11-08 21:31:56 +0900

    change message

--- hello.js
+++ hello.js
@@ -2,9 +2,7 @@ export default function handler(req, res) {
   const echo = req.query || {};
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.status(200).json({
-    userId: 1,
-    id: 1,
-    title: "delectus aut autem",
-    completed: false,
+    message: "Hello API",
+    data: [1, 2, 3],
   });
 }
